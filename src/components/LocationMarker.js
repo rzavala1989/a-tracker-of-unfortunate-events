43139dc4892c83138e3dc3e0deeedc2afabe4076
Ricardo Zavala <zavala.ricardo.m@gmail.com>
2022-02-10 02:09:42 -0600

    fixed old warnings

--- src/components/LocationMarker.js
+++ src/components/LocationMarker.js
@@ -5,53 +5,23 @@ import caneIcon from '@iconify/icons-mdi/hurricane';
 import bottleIcon from '@iconify/icons-mdi/bottle-wine';
 
 export const FireMarker = ({ lat, lng, onClick }) => {
-  const [hover, setHover] = useState(false);
-
-  const onHover = () => setHover(true);
-  const onLeave = () => setHover(false);
-
   return (
     <div className='fire-marker' onClick={onClick}>
-      <Icon
-        icon={fireIcon}
-        className='fire-icon'
-        onMouseEnter={onHover}
-        onMouseLeave={onLeave}
-      />
+      <Icon icon={fireIcon} className='fire-icon' />
     </div>
   );
 };
 export const CaneMarker = ({ lat, lng, onClick }) => {
-  const [hover, setHover] = useState(false);
-
-  const onHover = () => setHover(true);
-  const onLeave = () => setHover(false);
-
   return (
     <div className='cane-marker' onClick={onClick}>
-      <Icon
-        icon={caneIcon}
-        className='cane-icon'
-        onMouseEnter={onHover}
-        onMouseLeave={onLeave}
-      />
+      <Icon icon={caneIcon} className='cane-icon' />
     </div>
   );
 };
 export const VolcanoMarker = ({ lat, lng, onClick }) => {
-  const [hover, setHover] = useState(false);
-
-  const onHover = () => setHover(true);
-  const onLeave = () => setHover(false);
-
   return (
     <div className='volcano-marker' onClick={onClick}>
-      <Icon
-        icon={bottleIcon}
-        className='volcano-icon'
-        onMouseEnter={onHover}
-        onMouseLeave={onLeave}
-      />
+      <Icon icon={bottleIcon} className='volcano-icon' />
     </div>
   );
 };
